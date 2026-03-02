0a35b43ead9d7bd85f434eef0ed5f6dfe75c5e69
nicco8888 <nicksv88@gmail.com>
2026-03-01 23:12:15 -0500

    Initial commit

---
+++ SamplerateConverter.py
@@ -0,0 +1,18 @@
+# Hello GIT
+print("Hello World")
+
+# Path to the folder that this code will look in to find audio files
+audioSourcePath = ""
+# Path to the folder that this code will dump new audio files to
+audioDumpPath = ""
+# Desired samplerate for your audiofiles
+samplerateGoal = 0
+
+def checkSampleRate():
+    # Need to figure out how to check an audio files sample rate, but once I do, that will happen here
+
+def modifySamplerate():
+    # Function that modifies the sample rate of an audio file
+
+def saveToFolder():
+    # Takes an audio file that has the correct samplerate and simply saves a copy to the new folder
